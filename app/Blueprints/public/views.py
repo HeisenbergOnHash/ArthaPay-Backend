d4a2f481e61c758cf262b6d762aa4496dc9fcf8f
HeisenbergOnHash <heisenberg@syenacodelabs.com>
2024-12-05 07:54:52 +0000

    partially cleaned code and updating version naming

--- app/Blueprints/public/views.py
+++ app/Blueprints/public/views.py
@@ -6,7 +6,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_tok
 
 @public_blueprint.route('/', methods=['GET'])
 def public_info():
-    return jsonify(message="Public Information version 1.0 from LenDenPay", data = get_system_details()), 200
+    return jsonify(message="Public Information version 1.0.1 from LenDenPay", data = get_system_details()), 200
 
 @public_blueprint.route('/tokenRefresh', methods=['POST'])
 @jwt_required(refresh=True)
@@ -27,13 +27,9 @@ def callback():
 
     elif request.method == 'POST':
         try:
-            received_data = request.json 
-            if not received_data:
-                return jsonify(message="No data received in POST request"), 400
-
-            print(f"Received POST data: {received_data}")
-            return jsonify(message="Callback received via POST", data=received_data), 200
-
+            if not request.json:return jsonify(message="No data received in POST request"), 400
+            print(f"Received POST data: {request.json}")
+            return jsonify(message="Callback received via POST"), 200
         except Exception as e:
             print(f"Error processing POST data: {e}")
             return jsonify(message="Error processing POST request", error=str(e)), 500
